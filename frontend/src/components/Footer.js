4b16b3530bdb6dc91483196cf7ec155602d3e2ef
Alan Gamboa <alangam97@gmail.com>
2021-04-24 13:45:51 -0500

    styling

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <Container>
+                    <Col className ="text-center py-3">Copyright &copy; Proshop  </Col>
+                </Container>
+               
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
